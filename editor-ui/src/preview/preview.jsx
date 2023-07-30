6b5de00c940133e7b6fb67506159dc419057c27b
Florian Rüberg <florian.rueberg@gmail.com>
2023-07-30 21:34:05 +0200

    .

--- editor-ui/src/preview/preview.jsx
+++ editor-ui/src/preview/preview.jsx
@@ -6,12 +6,17 @@ import React from 'react';
 export function Preview() {
 
     function onRef(ref) {
-        panzoom(ref);
+        const instance = panzoom(ref);
+        instance.on('transform', function(e) {
+            console.log(e);
+            console.log(e.getTransform())
+        });
     }
 
-    return <div class="editor-preview">
-        <iframe ref={onRef} src="https://www.youtube.com"></iframe>
-        <div class="partially-hidden">
+    return <div className="editor-preview">
+        <div className="panzoom" ref={onRef}></div>
+        <iframe src="https://www.youtube.com"></iframe>
+        <div className="partially-hidden">
             <canvas></canvas>
         </div>
     </div>;
